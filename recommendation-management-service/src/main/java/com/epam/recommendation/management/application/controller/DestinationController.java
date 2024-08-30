712c8f075825f333101c45009a37340269c3035c
Aadithya Vikram <aadithya_vikram@epam.com>
2024-08-30 17:33:32 +0530

    added cross origin

--- recommendation-management-service/src/main/java/com/epam/recommendation/management/application/controller/DestinationController.java
+++ recommendation-management-service/src/main/java/com/epam/recommendation/management/application/controller/DestinationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("v1/destinations")
 public class DestinationController {
--- recommendation-management-service/src/main/java/com/epam/recommendation/management/application/controller/StateController.java
+++ recommendation-management-service/src/main/java/com/epam/recommendation/management/application/controller/StateController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("v1/states")
 public class StateController {
